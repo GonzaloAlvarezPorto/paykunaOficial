acff077a2efda38084bb12ad5fc84ad2fde438ef
GonzaloAlvarezPorto <gonzaalvarezporto@gmail.com>
2024-09-08 03:19:57 -0300

    Cambios en el mensaje de bienvenida

--- src/components/bMain/Welcome.jsx
+++ src/components/bMain/Welcome.jsx
@@ -3,41 +3,43 @@ import { useNavigate } from 'react-router-dom';
 
 export const Welcome = () => {
     const [linkFoto, setLinkFoto] = useState('/media/images/puertaCerradaBYN.png');
-    const [mensaje, setMensaje] = useState('Nos estamos preparando para vos')
+    const [mensaje, setMensaje] = useState('Estamos preparando todo para vos.')
     const [width, setWidth] = useState('');
     const navigate = useNavigate();
 
     useEffect(() => {
         const timer = setTimeout(() => {
             setLinkFoto('/media/images/puertaCerradaColor.png');
-            setMensaje('Estamos terminando de acomodar')
+            setMensaje('Estamos terminando de acomodar.')
         }, 2000);
 
         const secondTimer = setTimeout(() => {
-            setLinkFoto('/media/images/puertaAbierta.png');
-            setMensaje('Bienvenidxs a Paykuna')
+            setMensaje('Ya estamos, tocá la puerta para entrar.')
         }, 4000);
-
-        const thirdTimer = setTimeout(() => {
-            setLinkFoto('/media/images/paykuna.png')
-            setWidth('100px');
-        },5000)
-
-        const fourthTimer = setTimeout(()=> {
-            navigate('/home');
-        },6000)
 
         return () => {
             clearTimeout(timer);
             clearTimeout(secondTimer);
-            clearTimeout(thirdTimer);
-            clearTimeout(fourthTimer);
         };
     }, []);
 
+    const abrirPuerta = () => {
+        setLinkFoto('/media/images/puertaAbierta.png');
+        setMensaje('Bienvenide a Paykuna, estás siendo redireccionade.')
+        
+
+        const fourthTimer = setTimeout(() => {
+            navigate('/home');
+        }, 1500)
+
+        return () => {
+            clearTimeout(fourthTimer);
+        }
+    }
+
     return (
         <div className='contenedorBienvenida'>
-            <img className='puertaBienvenida' src={linkFoto} alt="puerta" style={{width:width}}/>
+            <img className='puertaBienvenida' src={linkFoto} alt="puerta" style={{ width: width }} onClick={abrirPuerta} />
             <p className='mensajeDeEstado'>{mensaje}</p>
         </div>
     );
